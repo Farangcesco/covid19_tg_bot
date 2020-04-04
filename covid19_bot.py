9de33bd9b64930f0983837f6b06369542f340ea9
Francesco Diomaiuta <francescodiomaiuta@Francescos-MBP.true.true>
2020-04-04 16:03:01 +0700

    add error handling for wrong Country code

--- covid19_bot.py
+++ covid19_bot.py
@@ -1,5 +1,6 @@
 import requests
 from call_covid_api import get_covid_data
+from country_code_list import is_valid_country
 
 #import logging
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
@@ -7,9 +8,13 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 token = '1107891137:AAE0111kLRMx5XDUWmrjCcoloov0jZgjVLI'
 
+
+
+
+
 def start(update, context):
     update.message.reply_text(
-        'Test'
+        'Give me a country to look up for. Has to be ISO country code, like TH, CH, BR'
     )
  #   return 'Initial'
 
@@ -24,9 +29,12 @@ def cancel(update, context):
 def echo(update, context):
     """Echo the user message."""
     user_message = update.message.text
-    display_data = get_covid_data(user_message)
-    
-    update.message.reply_text(display_data['global_data']['global_deaths'])
+    if is_valid_country(user_message):
+        display_data = get_covid_data(user_message)
+        update.message.reply_text(display_data['global_data']['global_deaths'])
+    else:
+        update.message.reply_text('Give me a country to look up for. Has to be ISO country code, like TH, CH, BR. Google it bro')
+        
 
 
 def get_data():
